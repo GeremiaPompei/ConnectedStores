79baef276a7a41aadea9b91045b9be02d634d27a
Geremia <geremia.pompei@studenti.unicam.it>
2021-02-11 16:45:02 +0100

    Finito java doc.

--- src/main/java/com/example/view/GUIViewController.java
+++ src/main/java/com/example/view/GUIViewController.java
@@ -21,43 +21,112 @@ import java.util.ResourceBundle;
 public class GUIViewController implements Initializable {
 
     /**
-     * Label utile per mostrare notifiche.
+     * Lista dei rec contenuti nella tabella.
+     */
+    private ObservableList<Rec.REC> olRec;
+
+    /**
+     * Tabella utile per visualizzare ed inserire i dati nel rec.
      */
     @FXML
-    Label notification;
+    TableView<Rec.REC> table;
+
+    /**
+     * Posizione del field nella tabella.
+     */
+    private int selectedField;
+
+    /**
+     * Posizione del rec nella tabella.
+     */
+    private int selectedRec;
+
+    /**
+     * Colonna degli id inerenti ai rec.
+     */
+    @FXML
+    TableColumn<Rec.REC, Integer> idColumn;
+
+    /**
+     * Rec su cui vengono settati i campi prima di essere inviato tramite una richiesta POST.
+     */
+    private RecEntity rec;
+
+    /**
+     * Campo di testo utile ad impostare il nome di un field.
+     */
+    @FXML
+    TextField fieldName;
+
+    /**
+     * Lista dei tipi di field.
+     */
+    private ObservableList<Integer> olFieldType;
+
+    /**
+     * Choice box utile per selezionare il tipo del field.
+     */
+    @FXML
+    ChoiceBox<Integer> fieldType;
+
+    /**
+     * Lista delle dimensioni di field.
+     */
+    private ObservableList<Integer> olFieldSize;
+
+    /**
+     * Choice box utile per selezionare la dimensione del field.
+     */
+    @FXML
+    ChoiceBox<Integer> fieldSize;
+
+    /**
+     * Campo testo utile per inserire il valore del rec.
+     */
+    @FXML
+    TextField recValue;
+
+    /**
+     * Campo testo utile per inserire il nome del rec.
+     */
+    @FXML
+    TextField recName;
+
+    /**
+     * Campo di testo utile per inserire l'indirizzo ip del del destinatario della richiesta POST.
+     */
+    @FXML
+    TextField ipAddressPost;
+    /**
+     * Campo di testo dell'indirizzo ip del destinatario della richiesta GET.
+     */
+    @FXML
+    TextField ipAddressGet;
+
+    /**
+     * Area di testo della risorsa ottenuta tramite la chiamata GET.
+     */
+    @FXML
+    TextArea clientArea;
 
     /**
      * Area di testo utile per mostrare i messaggi del server.
      */
     @FXML
     TextArea serverArea;
-    @FXML
-    TextField ipAddress;
-    @FXML
-    TextArea textArea;
-    private RecEntity rec;
-    private ObservableList<Integer> olFieldType;
-    private ObservableList<Integer> olFieldSize;
-    private ObservableList<Rec.REC> olRec;
-    @FXML
-    TextField fieldName;
-    @FXML
-    ChoiceBox<Integer> fieldType;
-    @FXML
-    ChoiceBox<Integer> fieldSize;
-    @FXML
-    TextField recValue;
-    @FXML
-    TextField recName;
-    @FXML
-    TextField ipReceiver;
-    @FXML
-    TableView<Rec.REC> table;
-    @FXML
-    TableColumn<Rec.REC, Integer> idColumn;
-    private int selectedField;
-    private int selectedRec;
 
+    /**
+     * Label utile per mostrare notifiche.
+     */
+    @FXML
+    Label notification;
+
+    /**
+     * Metodo inizializzatore.
+     *
+     * @param url
+     * @param resourceBundle
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         Controller.getInstance().init();
@@ -71,6 +140,9 @@ public class GUIViewController implements Initializable {
         init(1, 1);
     }
 
+    /**
+     * Metodo utile all'inizializzazione della choicebox fieldType.
+     */
     private void initFieldType() {
         olFieldType.add(rec.DEF_FLD_SZ);
         olFieldType.add(rec.DEF_DOUBLE_SZ);
@@ -81,6 +153,9 @@ public class GUIViewController implements Initializable {
         fieldType.setItems(olFieldType);
     }
 
+    /**
+     * Metodo utile all'inizializzazione della choicebox fieldSize.
+     */
     private void initFieldSize() {
         olFieldSize.add(RecEntity.FDT_STR);
         olFieldSize.add(RecEntity.FDT_DOUBLE);
@@ -91,6 +166,12 @@ public class GUIViewController implements Initializable {
         fieldSize.setItems(olFieldSize);
     }
 
+    /**
+     * Metodo utile all'inizializzazione e l'aggiornamento del rec.
+     *
+     * @param fieldcount Nuomero dei field.
+     * @param reccount   Numero dei rec.
+     */
     private void init(int fieldcount, int reccount) {
         RecEntity rec = new RecEntity();
         rec.setName(this.rec.getName());
@@ -107,95 +188,147 @@ public class GUIViewController implements Initializable {
         updateTable();
     }
 
+    /**
+     * Metodo utile ad aggiornare la tabella in base al nuovo stato del rec.
+     */
+    private void updateTable() {
+        try {
+            olRec.removeAll(olRec);
+            olRec.addAll(this.rec.getRec());
+            table.setItems(olRec);
+            table.getColumns().removeAll(table.getColumns());
+            table.getColumns().add(idColumn);
+            idColumn.setCellValueFactory(cellData -> {
+                for (int i = 0; i < this.rec.getReccount(); i++)
+                    if (this.rec.getRec()[i].equals(cellData.getValue()))
+                        return new SimpleObjectProperty<>(i);
+                return null;
+            });
+            for (int i = 0; i < this.rec.getFldcount(); i++) {
+                TableColumn<Rec.REC, String> tc = new TableColumn<>();
+                tc.setText(this.rec.getFldName(i));
+                table.getColumns().add(tc);
+                final int x = i;
+                tc.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().FLDREC[x] == null ?
+                        "" :
+                        cellData.getValue().FLDREC[x].VALUE));
+            }
+        } catch (Exception e) {
+            notification.setText(e.getMessage());
+        }
+    }
+
+    /**
+     * Metodo utile per la selezione di una casella della tabella.
+     */
+    @FXML
+    public void selected() {
+        try {
+            Rec.REC rec = table.getSelectionModel().getSelectedItem();
+            if (!table.getItems().isEmpty() && rec != null) {
+                table.getFocusModel().focusedCellProperty().addListener((obs, oldVal, newVal) -> {
+                    if (newVal.getTableColumn() != null) {
+                        selectedField = newVal.getColumn() - 1;
+                        selectedRec = table.getSelectionModel().getFocusedIndex();
+                        this.recValue.setText(this.rec.getValue(selectedField, selectedRec));
+                        this.fieldName.setText(this.rec.getFldName(selectedField));
+                        this.fieldType.setValue(this.rec.getFldType(selectedField));
+                        this.fieldSize.setValue(this.rec.getFldSize(selectedField));
+                    }
+                });
+            }
+        } catch (Exception e) {
+            notification.setText(e.getMessage());
+        }
+    }
+
+    /**
+     * Aggiunta field.
+     */
     @FXML
     public void addField() {
         init(this.rec.getFldcount() + 1, this.rec.getReccount());
     }
 
+    /**
+     * Rimozione field.
+     */
     @FXML
     public void removeField() {
         if (this.rec.getFldcount() > 1)
             init(this.rec.getFldcount() - 1, this.rec.getReccount());
     }
 
+    /**
+     * Aggiunta rec.
+     */
     @FXML
     public void addRec() {
         init(this.rec.getFldcount(), this.rec.getReccount() + 1);
     }
 
+    /**
+     * Rimozione rec.
+     */
     @FXML
     public void removeRec() {
         if (this.rec.getReccount() > 1)
             init(this.rec.getFldcount(), this.rec.getReccount() - 1);
     }
 
+    /**
+     * Metodo utile a settare un field all'interno del rec.
+     */
+    public void setField() {
+        try {
+            if (!table.getItems().isEmpty() && rec != null) {
+                this.rec.setFldData(selectedField, this.fieldName.getText(), this.fieldType.getValue(), this.fieldSize.getValue());
+            }
+            updateTable();
+        } catch (Exception e) {
+            notification.setText(e.getMessage());
+        }
+    }
+
+    /**
+     * Metodo utile a settare il valore di un rec.
+     */
     @FXML
     public void setValue() {
-        if (!table.getItems().isEmpty() && rec != null) {
-            this.rec.setValue(selectedField, selectedRec, this.recValue.getText());
+        try {
+            if (!table.getItems().isEmpty() && rec != null) {
+                this.rec.setValue(selectedField, selectedRec, this.recValue.getText());
+            }
+            updateTable();
+        } catch (Exception e) {
+            notification.setText(e.getMessage());
         }
-        updateTable();
     }
 
+    /**
+     * Metodo utile ad inviare una richiesta POST.
+     */
     @FXML
-    public void send() {
+    public void post() {
         try {
             rec.setName(recName.getText());
-            Controller.getInstance().getClient().postRec(rec, "https://" + ipReceiver.getText() + ":8080/");
+            boolean b = Controller.getInstance().getClient().postRec(rec, "https://" + ipAddressPost.getText() + ":8080/");
+            if (!b) {
+                notification.setText("La richiesta POST non è andata a buon fine!");
+            }
         } catch (Exception e) {
-            e.printStackTrace();
+            notification.setText(e.getMessage());
         }
     }
 
-    @FXML
-    public void selected() {
-        Rec.REC rec = table.getSelectionModel().getSelectedItem();
-        if (!table.getItems().isEmpty() && rec != null) {
-            table.getFocusModel().focusedCellProperty().addListener((obs, oldVal, newVal) -> {
-                if (newVal.getTableColumn() != null) {
-                    selectedField = newVal.getColumn() - 1;
-                    selectedRec = table.getSelectionModel().getFocusedIndex();
-                    this.recValue.setText(this.rec.getValue(selectedField, selectedRec));
-                    this.fieldName.setText(this.rec.getFldName(selectedField));
-                    this.fieldType.setValue(this.rec.getFldType(selectedField));
-                    this.fieldSize.setValue(this.rec.getFldSize(selectedField));
-                }
-            });
+    /**
+     * Metodo utile ad inviare una richiesta GET.
+     */
+    public void get() {
+        try {
+            clientArea.setText(StringfyRec.stringOf(Controller.getInstance().getClient().getRec("https://" + ipAddressGet.getText() + ":8080/")));
+        } catch (Exception e) {
+            notification.setText(e.getMessage());
         }
-    }
-
-    private void updateTable() {
-        olRec.removeAll(olRec);
-        olRec.addAll(this.rec.getRec());
-        table.setItems(olRec);
-        table.getColumns().removeAll(table.getColumns());
-        table.getColumns().add(idColumn);
-        idColumn.setCellValueFactory(cellData -> {
-            for (int i = 0; i < this.rec.getReccount(); i++)
-                if (this.rec.getRec()[i].equals(cellData.getValue()))
-                    return new SimpleObjectProperty<>(i);
-            return null;
-        });
-        for (int i = 0; i < this.rec.getFldcount(); i++) {
-            TableColumn<Rec.REC, String> tc = new TableColumn<>();
-            tc.setText(this.rec.getFldName(i));
-            table.getColumns().add(tc);
-            final int x = i;
-            tc.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().FLDREC[x] == null ?
-                    "" :
-                    cellData.getValue().FLDREC[x].VALUE));
-        }
-    }
-
-    public void setField() {
-        if (!table.getItems().isEmpty() && rec != null) {
-            this.rec.setFldData(selectedField, this.fieldName.getText(), this.fieldType.getValue(), this.fieldSize.getValue());
-        }
-        updateTable();
-    }
-
-    public void done() {
-        textArea.setText(StringfyRec.stringOf(Controller.getInstance().getClient()
-                .getRec("https://" + ipAddress.getText() + ":8080/")));
     }
 }
